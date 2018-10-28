4271172eb8bcda4512a63ff4b72c647099286750
Taoh Green <greenmachine777@gmail.com>
2018-10-27 21:53:44 -0700

    try fix

--- server/index.js
+++ server/index.js
@@ -41,7 +41,7 @@ app.get("/autocomplete-yelp-suggestions", async (req, res) => {
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
